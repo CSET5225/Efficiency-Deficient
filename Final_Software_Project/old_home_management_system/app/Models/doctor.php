b17345efa4dde7ebd4b87d943f79ba27f6ae79bd
leor9 <leor91172@gmail.com>
2023-11-17 14:50:26 -0500

    made all of the models

---
+++ Final_Software_Project/old_home_management_system/app/Models/doctor.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class doctor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "Doctor_id",
+        "role_id",
+        "first_name",
+        "last_name",
+        "D.O.B",
+        "email",
+        "salary",
+        "approved"
+     ] ;
+}
---
+++ Final_Software_Project/old_home_management_system/app/Models/medication.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class medication extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "medicine_id",
+        "medicine_name"
+     ] ;
+}
