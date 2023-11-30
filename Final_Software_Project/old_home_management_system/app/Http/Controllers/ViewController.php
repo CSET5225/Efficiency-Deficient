069b4b69b8e68f0402d9c1b2986459771c590e99
BananaBoi1232 <martin1420z@comcast.net>
2023-11-30 12:42:30 -0500

    Changed how the family code generates and html

--- Final_Software_Project/old_home_management_system/app/Http/Controllers/ViewController.php
+++ Final_Software_Project/old_home_management_system/app/Http/Controllers/ViewController.php
@@ -4,10 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ViewController extends Controller
 {
     public function registrationFormView(){
-        return view("registration_form");
+        $family_code = $this->generateCode();
+        return view("registration_form",["family_code"=>$family_code]);
+    }
+
+    //Generates the family code
+    public function generateCode(){
+        $family_code = substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'), 0, 10);
+        if(DB::select("SELECT 'family_code' FROM Patients WHERE 'family_code' = '$family_code'")){
+            return $this->generateCode();
+        }
+        return $family_code;
     }
 }
